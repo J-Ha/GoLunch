c1111ca921b90a29b2564a7a010a2c371b8b65ef
J-Ha <j-ha@j-ha.de>
2018-10-06 16:16:47 +0200

    build dropdown

--- Main.go
+++ Main.go
@@ -4,7 +4,11 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"html/template"
+	"log"
+	"net/http"
 	"os"
+	"strings"
 
 	"github.com/go-redis/redis"
 	"github.com/shurcooL/graphql"
@@ -45,8 +49,21 @@ type Open struct {
 	End   string
 }
 
+type User struct {
+	Name string
+}
+
+type DropdownItem struct {
+	Name  string
+	Value string
+}
+
 func main() {
-	indexRestaurants()
+	user := getUser()
+	restaurant := ("Asia Imbiss")
+	subscribe(restaurant, user)
+	http.HandleFunc("/", generateWebsite)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func yelpSearch(limit int, offset int) {
@@ -88,14 +105,13 @@ func indexRestaurants() {
 			if err != nil {
 				// Handle error.
 			}
-			RedisClient("r/"+string(business.Name), b)
+			redisClient("r/"+string(business.Name), b)
 		}
 		offset = offset + limit
 	}
-
 }
 
-func RedisClient(key string, value []byte) {
+func redisClient(key string, value []byte) {
 	client := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "", // no password set
@@ -106,4 +122,76 @@ func RedisClient(key string, value []byte) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func redisGet(key string) *redis.StringCmd {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	values := client.Get(key)
+	return values
+}
+
+func redisGetKeys(prefix string) *redis.StringSliceCmd {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	keys := client.Keys(prefix)
+	return keys
+}
+
+func getUser() []byte {
+	username := "Julian"
+	m := User{
+		username,
+	}
+	b, err := json.Marshal(m)
+	if err != nil {
+	}
+	return b
+}
+
+func subscribe(restaurant string, user []byte) {
+	redisClient("s/"+restaurant, user)
+}
+
+func generateWebsite(w http.ResponseWriter, r *http.Request) {
+	html := `<!DOCTYPE html>
+	<html>
+	<body>
+   
+	<select> // for loop in html template example
+	  {{range $key, $value := .}}
+		<option value="{{ $value }}">{{ $key }}</option>
+	  {{end}}
+   
+   
+	</select>
+   
+	</body>
+	</html>`
+
+	names, _ := redisGetKeys("r/*").Result()
+
+	for num, rest := range names {
+		values, _ := redisGet(rest).Result()
+		var htmlrest = map[string]interface{}{
+			strings.Replace(names[num], "r/", "", -1): values,
+		}
+		//io.WriteString(w, strings.Replace(names[num], "r/", "", -1)+" "+values+"\n")
+		dropdownTemplate, err := template.New("dropdownexample").Parse(string(html))
+		if err != nil {
+			panic(err)
+		}
+		// populate dropdown with fruits
+		dropdownTemplate.Execute(w, htmlrest)
+
+	}
+
 }
