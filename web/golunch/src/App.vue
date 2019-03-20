20033a32712945e67a8a52890d7ff92946995312
J-Ha <j-ha@j-ha.de>
2019-03-20 08:20:21 +0100

    [POC] Vue.js Frontend

---
+++ web/golunch/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Go</span>
+        <span class="font-weight-light">Lunch</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/J-Ha/GoLunch"
+        target="_blank"
+      >
+        <span class="mr-2">Alpha Release</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      //
+    }
+  }
+}
+</script>
